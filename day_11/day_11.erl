79cdf71ccb028ec1f52d4b5958f4805f83028085
Tyler Church <tylertrain@yahoo.com>
2022-12-13 06:50:27 -0700

    Day 11 - Part 2. After spending a ton of time way overthinking things, a simple modulus of the LCM to the rescue.

--- day_11/day_11.erl
+++ day_11/day_11.erl
@@ -2,6 +2,7 @@
 -export([day_11_part_1/1, day_11_part_2/1]).
 
 -record(monkey, {id, items=[], inspection_count, operator, operand, test_value, true_monkey, false_monkey}).
+-record(item, {id, worry_level, monkey_id, path}).
 
 monkey_parse(Section) ->
   [
@@ -84,7 +85,6 @@ round_evaluate(Monkeys, Evaluator) ->
 rounds_evaluate(0, Monkeys, _) ->
   Monkeys;
 rounds_evaluate(N, Monkeys, Evaluator) ->
-  io:format("Rounds remaining: ~p\n", [N]),
   rounds_evaluate(N - 1, round_evaluate(Monkeys, Evaluator), Evaluator).
 
 day_11_part_1(FilePath) ->
@@ -95,70 +95,103 @@ day_11_part_1(FilePath) ->
   [M1, M2 | _] = lists:reverse(lists:keysort(#monkey.inspection_count, MonkeysAfter20)),
   M1#monkey.inspection_count * M2#monkey.inspection_count.
 
-monkey_evaluate_faster(Monkey, Monkeys) ->
-  ItemsTransferred = lists:map(
-    fun (WorryLevelInitial) ->
-      Operand = case Monkey#monkey.operand of
-                  old -> WorryLevelInitial;
-                  _ -> Monkey#monkey.operand
-                end,
-      WorryLevelTransformed = case Monkey#monkey.operator of
-                                '+' -> WorryLevelInitial + Operand;
-                                '*' -> WorryLevelInitial * Operand
-                              end,
-%%      WorryLevelFinal = floor(WorryLevelTransformed / 3),
-      WorryLevelFinal = WorryLevelTransformed,
-      io:format("~p\n", [WorryLevelFinal rem Monkey#monkey.test_value]),
-      case (WorryLevelFinal rem Monkey#monkey.test_value) == 0 of
-        true -> {Monkey#monkey.true_monkey, WorryLevelFinal div Monkey#monkey.test_value};
-        false -> {Monkey#monkey.false_monkey, WorryLevelFinal}
+monkey_evaluate_part_2(Monkey, Monkeys, Items, Lcm) ->
+  {ItemsUpdated, InspectionCount} = lists:mapfoldl(
+    fun (Item, Accum) ->
+      case Item#item.monkey_id == Monkey#monkey.id of
+        false -> {Item, Accum};
+        true ->
+          WorryLevelInitial = Item#item.worry_level,
+          Operand = case Monkey#monkey.operand of
+                      old -> WorryLevelInitial;
+                      _ -> Monkey#monkey.operand
+                    end,
+          WorryLevelTransformed = case Monkey#monkey.operator of
+                                    '+' -> WorryLevelInitial + Operand;
+                                    '*' -> WorryLevelInitial * Operand
+                                  end,
+          WorryLevelFinal = WorryLevelTransformed,
+          if
+            WorryLevelFinal > 18446744073709551615 -> throw(number_too_big);
+            true -> ok
+          end,
+          ItemNext = case (WorryLevelFinal rem Monkey#monkey.test_value) == 0 of
+            true -> Item#item{
+              monkey_id = Monkey#monkey.true_monkey,
+              worry_level = WorryLevelFinal rem Lcm,
+              path = [ {Monkey#monkey.id, true, WorryLevelFinal, Monkey#monkey.operand} | Item#item.path ]
+            };
+            false -> Item#item{
+              monkey_id = Monkey#monkey.false_monkey,
+              worry_level = WorryLevelFinal rem Lcm,
+              path = [ {Monkey#monkey.id, false, WorryLevelFinal, Monkey#monkey.operand} | Item#item.path ]
+            }
+          end,
+          {ItemNext, Accum + 1}
       end
     end,
-    Monkey#monkey.items
+    0,
+    Items
   ),
   MonkeyNext = Monkey#monkey{
     items = [],
-    inspection_count = Monkey#monkey.inspection_count + length(Monkey#monkey.items)
+    inspection_count = Monkey#monkey.inspection_count + InspectionCount
   },
-  lists:map(
+  MonkeysUpdated = lists:map(
     fun (MonkeyOld) ->
       if
         MonkeyOld == Monkey -> MonkeyNext;
-        true ->
-          RelevantItems = lists:filtermap(
-            fun ({MonkeyId, Item}) ->
-              if
-                MonkeyId == MonkeyOld#monkey.id -> {true, Item};
-                true -> false
-              end
-            end,
-            ItemsTransferred
-          ),
-          MonkeyOld#monkey{
-            items = MonkeyOld#monkey.items ++ RelevantItems
-          }
+        true -> MonkeyOld
       end
     end,
     Monkeys
+  ),
+  {MonkeysUpdated, ItemsUpdated}.
+
+round_evaluate_part2(Monkeys, Items, Lcm, Evaluator) ->
+  lists:foldl(
+    fun (#monkey{ id = MonkeyId }, {MonkeysLatest, ItemsLatest}) ->
+      Monkey = lists:keyfind(MonkeyId, #monkey.id, MonkeysLatest),
+      Evaluator(Monkey, MonkeysLatest, ItemsLatest, Lcm)
+    end,
+    {Monkeys, Items},
+    Monkeys
   ).
+
+rounds_evaluate_part2(0, Monkeys, Items, _, _) ->
+  {Monkeys, Items};
+rounds_evaluate_part2(N, Monkeys, Items, Lcm, Evaluator) ->
+  {MonkeysNext, ItemsNext} = round_evaluate_part2(Monkeys, Items, Lcm, Evaluator),
+  rounds_evaluate_part2(N - 1, MonkeysNext, ItemsNext, Lcm, Evaluator).
 
 day_11_part_2(FilePath) ->
   {ok, Text} = file:read_file(FilePath),
   Sections = string:split(Text, "\n\n", all),
   Monkeys = lists:map(fun monkey_parse/1, Sections),
-  MonkeysAfter10000 = rounds_evaluate(10000, Monkeys, fun monkey_evaluate_faster/2),
+  Items = lists:foldl(
+    fun (Monkey, Accum) ->
+      Accum ++ lists:map(
+        fun (ItemInitialValue) ->
+          #item{
+            id = ItemInitialValue,
+            worry_level = ItemInitialValue,
+            monkey_id = Monkey#monkey.id,
+            path = []
+          }
+        end,
+        Monkey#monkey.items
+      )
+    end,
+    [],
+    Monkeys
+  ),
+  Lcm = lists:foldl(
+    fun (#monkey{ test_value = TestValue }, Accum) ->
+      TestValue * Accum
+    end,
+    1,
+    Monkeys
+  ),
+  {MonkeysAfter10000, _} = rounds_evaluate_part2(10000, Monkeys, Items, Lcm, fun monkey_evaluate_part_2/4),
   [M1, M2 | _] = lists:reverse(lists:keysort(#monkey.inspection_count, MonkeysAfter10000)),
   M1#monkey.inspection_count * M2#monkey.inspection_count.
-
-% Here's what I know:
-%
-% The monkeys who add are shifting along a possibility space, and the total magnitude of the number
-% doesn't matter, only how close it is to the divisor.
-%
-% The monkeys who are multiplying by primes could in theory be opening things up for future monkeys
-% i.e. by then making the item a multiple of 19.
-% As far as I can tell they aren't opening up their own possibility space _at all_ (except indirectly,
-% through knock-on effects from later monkeys, perhaps).
-%
-% I'm uncertain what the monkeys who are multiplying by the current value are doing... It almost
-% seems as if it should be a no-op, but that isn't borne out by my tests.
